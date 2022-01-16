106f1677650c7318a3acf1b5c0de83a1384edfd5
Manatsa Chinyeruse <manatsachinyeruse@gmail.com>
2022-01-16 21:13:06 +0200

    first commit

---
+++ app/src/main/java/com/mana/wizard/TutorialActivity.java
@@ -0,0 +1,13 @@
+package com.mana.wizard;
+
+import android.os.Bundle;
+import androidx.fragment.app.FragmentActivity;
+
+public class TutorialActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+    }
+}
